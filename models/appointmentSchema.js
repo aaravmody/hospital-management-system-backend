9494c0445b0cce73247ec62d94dee7dfbcb15e8b
aaravmody <aaravmody2@gmail.com>
2024-06-15 00:49:19 +0530

    backend

---
+++ models/appointmentSchema.js
@@ -0,0 +1,83 @@
+import mongoose from "mongoose";
+import validator from "validator";
+import bcrypt from 'bcrypt'
+import jwt from 'jsonwebtoken'
+
+const appointmentSchema = new mongoose.Schema({
+    firstName:{
+        type: String,
+        required: true,
+        minLength: [3,"First name must be atleast 3 characters"]
+    },
+    lastName:{
+        type: String,
+        required: true,
+        minLength: [3,"Last name must be atleast 3 characters"]
+    },
+    email:{
+        type: String,
+        required: true,
+        validate: [validator.isEmail, "Please provide a valid email"]
+    },
+    phone:{
+        type: String,
+        required: true,
+        minLength: [10,"Phone number should be 10 numbers long"]
+    },
+    id:{
+        type: String,
+        required: true,
+        minLength: [10,"Id must contain exactly 10 characters"],
+        maxLength: [10,"Id must contain exactly 10 characters"]
+    },
+    dob:{
+        type: Date,
+        required: [true,"DOB is required"]
+    },
+    gender:{
+        type: String,
+        required: true,
+        enum: ["Male", "Female"]
+    },
+    appointment_date:{
+        type: String,
+        required: true
+    },
+    department:{
+        type: String,
+        required: true
+    },
+    doctor:{
+        firstName:{
+            type: String,
+            required: true
+        },
+        lastName:{
+            type: String,
+            required: true
+        }
+    },
+    hasVisited:{
+        type: Boolean,
+        default: false
+    },
+    doctorId:{
+        type: mongoose.Schema.ObjectId,
+        required: true
+    },
+    patientId:{
+        type: mongoose.Schema.ObjectId,
+        required: true
+    },
+    address:{
+        type: String,
+        required: true
+    },
+    status:{
+        type: String,
+        enum: ["Pending","Accepted","Rejected"],
+        default: "Pending"
+    },
+})
+
+export const Appointment = mongoose.model("Appointment",appointmentSchema);
